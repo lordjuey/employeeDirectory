94951c910889e5cf30243daddac7c40c15d6f200
Juwon <lordjuey@gmail.com>
2020-10-20 20:28:04 +1100

    app.js polished

--- employee_directory/src/App.js
+++ employee_directory/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Hdeader';
+import EmployeeTable from './components/EmbloyeeTable';
+import Employees from "./employeeList.json";
 
-function App() {
+const App = () => {
+
+  const [employeeList] = React.useState(Employees);
+  const [searchVal, setSearchVal] = React.useState('');
+
+  const searchHandler = (event) => {
+    setSearchVal(event.target.value);
+  }
+
+  const searchResult = employeeList.filter(result => {
+    const searchByName = result.name.toLowerCase().includes(searchVal.toLowerCase());
+    const searchByOccu = result.occupation.toLowerCase().includes(searchVal.toLowerCase());
+    const searchByEmail = result.email.toLowerCase().includes(searchVal.toLowerCase());
+    const searchByPhone = result.phone.toLowerCase().includes(searchVal.toLowerCase());
+    return searchByName || searchByOccu || searchByEmail || searchByPhone ;
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <TableTitle onSearch={searchHandler} search={searchVal}/>
+      <EmployeeTable employeeList = {searchResult}/>
     </div>
-  );
+  )
+
 }
+
+
 
 export default App;
