eccaac6fe3b7b92121ea4f6cfcb3b7b13f0823eb
Juwon <lordjuey@gmail.com>
2020-10-30 00:45:55 +1100

    Updates

--- precache-manifest.748f3bfc42f9b028ac01082205a38c2f.js
+++ precache-manifest.748f3bfc42f9b028ac01082205a38c2f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2b2c1fc888f8655304fe9205ff09d493",
+    "revision": "185a39393e7dc6b62a1fe5cc18361492",
     "url": "/employee_directory/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/employee_directory/static/css/2.2a86faee.chunk.css"
   },
   {
-    "revision": "79fa7719b55a6082670f",
+    "revision": "d62ad5a0445e3ee5f2dc",
     "url": "/employee_directory/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/employee_directory/static/js/2.2c530b2e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "79fa7719b55a6082670f",
-    "url": "/employee_directory/static/js/main.4b4bb38d.chunk.js"
+    "revision": "d62ad5a0445e3ee5f2dc",
+    "url": "/employee_directory/static/js/main.970a6947.chunk.js"
   },
   {
     "revision": "498fd09be4c9f38769b0",
