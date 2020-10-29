d9e968c1ad55befd4cc0e3196395700f8f033d2b
Juwon <lordjuey@gmail.com>
2020-10-30 00:12:52 +1100

    fixing

---
+++ employee_directory/build/precache-manifest.9111f5cac6607dd75b40117498fa55ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba2f493b95eb194c190824fadc218211",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41b9feacd73aa0161134",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "79fa7719b55a6082670f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "41b9feacd73aa0161134",
+    "url": "/static/js/2.2c530b2e.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.2c530b2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79fa7719b55a6082670f",
+    "url": "/static/js/main.4b4bb38d.chunk.js"
+  },
+  {
+    "revision": "5b446e10af4fafd5a747",
+    "url": "/static/js/runtime-main.f71b05c2.js"
+  }
+]);
